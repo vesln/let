7cafd64095b8e1b60762bef69a9945ba83224226
Veselin Todorov <veselin@veselin.bg>
2012-02-12 20:03:30 +0200

    More app tests.

--- test/app.test.js
+++ test/app.test.js
@@ -12,16 +12,17 @@ var jack = require('jack');
 var should = require('chai').should();
 var flatiron = require('flatiron');
 
-console.log(flatiron);
 /**
  * Context.
  */
-var app = require('../lib/app.js');
+var app = require('../lib/app');
 
 describe('app', function() {
   it('should be a flatiron app', function() {
     app.should.eql(flatiron.app);
   });
+
+  it('should use the cli plugin', function() {
+    app.plugins.cli.should.be.ok
+  });
 });
-
-
